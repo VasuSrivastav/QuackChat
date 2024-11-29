71969a90897e2d8ccb69d9f0679ebdc3dacb1e27
VasuSrivastav <115205203+VasuSrivastav@users.noreply.github.com>
2024-11-30 03:01:47 +0530

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -113,6 +113,6 @@ export const checkAuth = (req, res) => {
     res.status(200).json(req.user);
   } catch (error) {
     console.log("Error in checkAuth controller", error.message);
-    res.status(500).json({ message: "Internal server error cloudinary only jpg allowed" });
+    res.status(500).json({ message: "Internal server error" });
   }
 };
